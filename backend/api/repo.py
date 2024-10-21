9ddbaaf9755e82f0f9a9e0b5d255bb2a6195f6d7
Roshan Manjushree Adhikari <manjushreeroshan@gmail.com>
2024-10-21 12:06:53 +0545

    note and reminder reln one to one defined in model itself

--- backend/api/repo.py
+++ backend/api/repo.py
@@ -38,7 +38,16 @@ class Reminder(Base):
     reminder_time = Column(DateTime(), nullable=False)
     note_id = Column(Integer, ForeignKey('notes.id'))
 
-    note = relationship("Note")
+    note = relationship("Note", back_populates="reminder")
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'email' : self.email,
+            'message' : self.message,
+            'reminder_time' : self.reminder_time,
+            'reminder_time_readable' : humanize.naturaltime(self.reminder_time) if self.reminder_time else None,
+        }
 
 class Note(Base):
     __tablename__ = 'notes'
@@ -49,7 +58,7 @@ class Note(Base):
     created_on = Column(DateTime(), default=datetime.now)
     updated_on = Column(DateTime(), default=datetime.now, onupdate=datetime.now)
 
-    reminder = relationship("Reminder")
+    reminder = relationship("Reminder", uselist=False, back_populates="note")
 
     def to_dict(self):
         return {
@@ -59,6 +68,8 @@ class Note(Base):
             'created_at'         : self.created_on,
             'updated_on'         : self.updated_on,
             'updated_at_readable': humanize.naturaltime(self.updated_on) if self.updated_on else None,
+
+            'reminder'           : self.reminder.to_dict() if self.reminder else None,            
         }
 
 
