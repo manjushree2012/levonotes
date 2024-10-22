e0d3731c4a12b051c37c25d317e12e73a3e54984
Roshan Manjushree Adhikari <manjushreeroshan@gmail.com>
2024-10-22 16:50:11 +0545

    bugfix

--- backend/domains/note/repo/models/note.py
+++ backend/domains/note/repo/models/note.py
@@ -28,4 +28,24 @@ class Note(Base):
             'reminder'           : self.reminder.to_dict() if self.reminder else None,            
         }
 
+class Reminder(Base):
+    __tablename__ = 'reminders'
+
+    id = Column(Integer(), primary_key=True)
+    email = Column(String(100), nullable=False)
+    message = Column(Text, nullable=False)
+    reminder_time = Column(DateTime(), nullable=False)
+    note_id = Column(Integer, ForeignKey('notes.id'))
+
+    note = relationship("Note", back_populates="reminder")
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'email' : self.email,
+            'message' : self.message,
+            'reminder_time' : self.reminder_time,
+            'reminder_time_readable' : humanize.naturaltime(self.reminder_time) if self.reminder_time else None,
+        }
+
 
