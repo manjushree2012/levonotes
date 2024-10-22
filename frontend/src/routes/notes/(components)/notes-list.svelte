4e334b3b609fabb3c2a22d497cb0a482e32f204a
Roshan Manjushree Adhikari <manjushreeroshan@gmail.com>
2024-10-22 14:37:51 +0545

    moved note list to separate component

---
+++ frontend/src/routes/notes/(components)/notes-list.svelte
@@ -0,0 +1,37 @@
+<script>
+    export let mails = [];
+    export let selectedNoteId;
+    export let selectMail;
+</script>
+
+<div class="flex flex-col gap-2 p-4 pt-0">
+    {#each mails as item}
+        <button 
+        class={`hover:bg-accent flex flex-col items-start gap-2 rounded-lg border p-3 text-left text-sm transition-all ${$selectedNoteId === item.id ? 'bg-muted' : ''}`} 
+        on:click={() => selectMail(item.id) }>
+            <div class="flex w-full flex-col gap-1">
+                <div class="flex items-center">
+                    <div class="flex items-center gap-2">                                               
+                    </div>
+
+                    <!-- ml-auto text-xs text-muted-foreground -->
+                     <!-- Add this class if selcted or smething for time wala class -->
+                    <div class="ml-auto text-xs text-foreground">
+                        { item.updated_at_readable }
+                    </div>
+                </div>
+            </div>
+            <div class="text-muted-foreground line-clamp-2 text-xs">
+                {item.content.substring(0, 300)}
+            </div>
+            {#if item.reminder}
+                <div class="flex items-center gap-2">
+                    <!-- <Badge>
+                        <BellRing class="mr-2 h-4 w-4" />
+                         Remind me:  {item.reminder.reminder_time_readable} 
+                    </Badge> -->
+                </div>
+            {/if }
+        </button>
+    {/each}
+</div>
--- frontend/src/routes/notes/+page.svelte
+++ frontend/src/routes/notes/+page.svelte
@@ -1,6 +1,7 @@
 <script>
     import { onMount } from 'svelte';
     import Tiptap from '$lib/Tiptap.svelte'
+    import NotesList from './(components)/notes-list.svelte';
 
     import { Search } from 'lucide-svelte';
     import { X } from 'lucide-svelte';
@@ -303,36 +304,7 @@
                     <!-- Notes List Start -->
 
                     <ScrollArea class="h-screen">
-                        <div class="flex flex-col gap-2 p-4 pt-0">
-                            {#each mails as item}
-                                <button 
-                                class={`hover:bg-accent flex flex-col items-start gap-2 rounded-lg border p-3 text-left text-sm transition-all ${$selectedNoteId === item.id ? 'bg-muted' : ''}`} 
-                                on:click={() => selectMail(item.id) }>
-                                    <div class="flex w-full flex-col gap-1">
-                                        <div class="flex items-center">
-                                            <div class="flex items-center gap-2">                                               
-                                            </div>
-
-                                            <!-- ml-auto text-xs text-muted-foreground -->
-                                             <!-- Add this class if selcted or smething for time wala class -->
-                                            <div class="ml-auto text-xs text-foreground">
-                                                { item.updated_at_readable }
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div class="text-muted-foreground line-clamp-2 text-xs">
-                                        {item.content.substring(0, 300)}
-                                    </div>
-                                    {#if item.reminder}
-                                        <div class="flex items-center gap-2">
-                                            <Badge>
-                                                <BellRing class="mr-2 h-4 w-4" />
-                                                 Remind me:  {item.reminder.reminder_time_readable} </Badge>
-                                        </div>
-                                    {/if }
-                                </button>
-                            {/each}
-                        </div>
+                       <NotesList {mails} {selectedNoteId} {selectMail} />
                     </ScrollArea>
 				</Tabs.Content>
 			</Tabs.Root>
